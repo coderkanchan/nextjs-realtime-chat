e13265e3d455d393ada231a48cf8bed6db4fff70
kanchan sharma <kanusharma881388@gmail.com>
2026-01-15 13:47:28 +0530

    Initial live chat app

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,13 +1,3 @@
-// import { NextResponse } from "next/server";
-
-// export async function POST() {
-//   const res = NextResponse.json({ message: "Logged out" });
-//   res.cookies.set("token", "", { maxAge: 0 });
-//   return res;
-// }
-
-
-
 
 import { NextResponse } from "next/server";
 
