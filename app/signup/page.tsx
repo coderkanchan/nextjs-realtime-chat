d6c149c40850713b554e1b8d4307cc394594bb97
kanchan sharma <kanusharma881388@gmail.com>
2026-05-26 21:59:25 +0530

    changed the sign up forms style

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -65,7 +65,7 @@ export default function SignupPage() {
           </h2>
 
           <input
-            className="w-full p-4 bg-black border border-gray-700 rounded-xl text-white outline-none focus:ring-2 focus:ring-blue-500/50 transition-all"
+            className="w-full p-4 bg-white border border-gray-700 rounded-xl text-black outline-none focus:ring-2 focus:ring-blue-500/50 transition-all"
             placeholder="Username"
             onChange={(e) => setForm({ ...form, username: e.target.value })}
             required
@@ -73,7 +73,7 @@ export default function SignupPage() {
 
           <input
             type="email"
-            className="w-full border p-3 rounded-lg mb-4 text-gray-700 outline-none"
+            className="w-full p-4 bg-black border border-gray-700 rounded-xl text-white outline-none focus:ring-2 focus:ring-blue-500/50 transition-all"
             placeholder="Email"
             onChange={(e) => setForm({ ...form, email: e.target.value })}
             required
